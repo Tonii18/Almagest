079e5725623b7c9ce1a8fadcf4cc8843c29f2fd4
Tonii18 <ajsanrosal@gmail.com>
2025-10-27 13:00:35 +0100

    Migrations and models

---
+++ app/Models/Contain_art_delivlines.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contain_art_delivlines extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Contains_art_invlines.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contains_art_invlines extends Model
+{
+    use HasFactory;
+}
