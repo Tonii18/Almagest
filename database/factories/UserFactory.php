1de1a7e05193d61da040b372b559a7730629bbe7
Jesus <gomez.rojes22@cadiz.salesianos.edu>
2025-10-28 18:32:58 +0100

    Admin Factory and Admin Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,18 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'firstname' => fake()->firstName(),
+        'secondname' => fake()->lastName(),
+        'email' => fake()->unique()->safeEmail(),
+        'email_verified_at' => now(),
+        'password' => static::$password ??= Hash::make('password'),
+        'company_id' => null,
+        'type' => 'user',
+        'email_confirmed' => 0,
+        'activated' => 0,
+        'iscontact' => 0,
+        'deleted' => 0,
+        'remember_token' => Str::random(10),
         ];
     }
 
@@ -40,5 +47,18 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function admin(){
+        return $this->state(fn(array $attributes) => [
+        'type' => 'admin',
+        'email' => 'admin@admin.com',
+        'password' => Hash::make('12345678'),
+        'firstname' => 'Administrador',
+        'secondname' => 'Administrador',
+        'activated' => 1,
+        'email_confirmed' => 1,
+        'email_verified_at' => now(),
+    ]);
     }
 }
