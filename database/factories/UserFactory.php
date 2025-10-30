8aaa8bb1e17feff98763f85ddb57e9b33c5c64ad
Jesus <gomez.rojes22@cadiz.salesianos.edu>
2025-10-30 09:05:05 +0100

    Changes UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
         'email' => fake()->unique()->safeEmail(),
         'email_verified_at' => now(),
         'password' => static::$password ??= Hash::make('password'),
-        'company_id' => null,
+        'company_id' => \App\Models\Company::all()->random()->id,
         'type' => 'user',
         'email_confirmed' => 0,
         'activated' => 0,
